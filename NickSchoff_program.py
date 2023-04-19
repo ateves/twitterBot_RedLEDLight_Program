572d9fe829c6b86133278b675d973e2b92b7cfd6
ateves <39039918+ateves@users.noreply.github.com>
2023-04-19 11:01:40 -0400

    jamquest draft
    added nick's jamquest code

---
+++ NickSchoff_program.py
@@ -0,0 +1,13 @@
+import random
+
+heroNameList = ["Blue Eyes White Dragon", "Cyclone Supersuit Carl", "Gregorian Gym Man", "Karate Kenny Kid", "Liga Lunar Luchador", "Parking Meter Pyro", "Soiree Senorita de Spyderman", "Weight Wordy Wordsmith"]
+
+def displayNow(heroNameList):
+    
+    minimumAmount = 5
+    
+    for x in heroNameList:
+        currentRandNumber = random.randint(minimumAmount,100)
+        print("%s donated $%s dollars!" % (x, currentRandNumber))
+
+displayNow(heroNameList)
